9b6cd8a7d3ba40dd9662628035fc97e9ec0cb991
Javia Holness <jholness97@gmail.com>
2022-10-19 17:22:38 -0400

    commit tree e4163096dad530d5414bca327d20e4cf3edf9a51

---
+++ index.js
@@ -0,0 +1,7 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  function introduction(firstName = "Aki", "Saimp") {
+    console.log(`Hello, ${firstName}!`);
+    return "Hi, my name is Aki.", "Hi, my name is Saimp"
+  }
